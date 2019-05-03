f725f3b734e197a7b22b32db39a696822637f1d0
1derless <fr3ncham@btinternet.com>
2019-05-03 20:50:48 +0100

    Major refactor of 'lib.rs' to use Rayon. + slight performance improvement + rewritten to perform all the Mandelbrot calculations before any colouring which is now done on the main thread + rewritten to use Rayon instead of the previous combinations of `crate::region`, `std::thread`, and `std::sync` crates,making the code simpler and easier to read as a result  (We use Rust for a reason, after all.) - removed 'region.rs' as it is now redundant + added a small fix to the smoothing algorithm, which is now optional at compile to through disabling the `smoothing` feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,18 @@
 mod configuration;
 mod mandelbrot;
-mod region;
 
 pub use configuration::Configuration;
 pub use mandelbrot::{iterate_mandelbrot, MandelResult};
-use region::Region;
 
-use image::imageops::replace;
 use image::RgbImage;
 use num_complex::Complex;
 use palette::{Gradient, LinSrgb};
-use std::sync::mpsc::{channel, Sender};
-use std::sync::{Arc, Mutex};
-use std::thread;
-
-pub fn mandelbrot_loop(
-    regions: Arc<Mutex<Vec<Region>>>,
-    config: Configuration,
-    grad_func: &(Fn(MandelResult) -> image::Rgb<u8>),
-    tx: Sender<(
-        Region,
-        image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>>,
-    )>,
-) {
-    // Loops until there are no more regions in the shared Vector.
-    loop {
-        let region = match regions.lock() {
-            Ok(mut regions) => match regions.pop() {
-                Some(region) => region,
-                None => break,
-            },
-            Err(_) => continue,
-        };
-
-        let mut img = RgbImage::new(region.w, region.h);
-        for (x, y, pixel) in img.enumerate_pixels_mut() {
-            let c = Complex::new(
-                (region.x + x) as f64 / config.output_w as f64 * (config.x_max - config.x_min)
-                    + config.x_min,
-                (region.y + y) as f64 / config.output_h as f64 * (config.y_max - config.y_min)
-                    + config.y_min,
-            );
-
-            *pixel = grad_func(iterate_mandelbrot(c));
-        }
-        tx.send((region, img)).unwrap();
-    }
-}
+use rayon::prelude::*;
 
 pub fn run(config: Configuration) {
     // The gradient isn't constant because its type is complicated.
     let grad = Gradient::new(vec![
-        /*LinSrgb::new(1.0, 0.0, 0.0),
+        /* LinSrgb::new(1.0, 0.0, 0.0),
         LinSrgb::new(0.1, 0.1, 0.1),
         LinSrgb::new(0.0, 1.0, 0.0),
         LinSrgb::new(0.1, 0.1, 0.1),
@@ -66,50 +27,48 @@ pub fn run(config: Configuration) {
         LinSrgb::new(1.0, 1.0, 1.0),
     ]);
 
-    // Creates tiny views of the mandelbrot set fractal and puts them in a thread-safe vector.
-    let output_region = Region::new(config.output_w, config.output_h);
-    let regions = Arc::new(Mutex::new(
-        output_region.clone().split(Region::new(256, 256)),
-    ));
-    let no_regions = regions.lock().unwrap().len();
+    //todo: separate below into distinct functions.
 
-    // Creates the shared objects.
-    let (tx, rx) = channel();
-    let mut handles = vec![];
+    // Iterates over all coordinates, in parallel.
+    let mut results: Vec<MandelResult> = (0..config.output_h)
+        .into_par_iter()
+        .flat_map(|y| {
+            (0..config.output_w)
+                .into_par_iter()
+                .rev()
+                .map(move |x| (x, y))
+        })
+        .map(|(x, y)| {
+            let c = Complex::new(
+                x as f64 / config.output_w as f64 * (config.x_max - config.x_min) + config.x_min,
+                y as f64 / config.output_h as f64 * (config.y_max - config.y_min) + config.y_min,
+            );
 
-    // Initialises and starts the threads.
-    for _ in 0..num_cpus::get() {
-        let tx = tx.clone();
-        let grad = grad.clone();
-        let regions = Arc::clone(&regions);
-        let config = config.clone();
+            iterate_mandelbrot(c)
+        })
+        .collect();
 
-        let colour = move |c| match c {
-            MandelResult::Outside(i) => {
-                let components = grad.get((i / 25.0) % 1.0);
-                image::Rgb([
-                    (components.red * 255.0) as u8,
-                    (components.green * 255.0) as u8,
-                    (components.blue * 255.0) as u8,
-                ])
-            }
-            MandelResult::Inside => image::Rgb([0u8, 0u8, 0u8]),
-        };
+    let mut master_image = RgbImage::new(config.output_w, config.output_h);
 
-        let handle = thread::spawn(move || mandelbrot_loop(regions, config, &colour, tx));
-        handles.push(handle);
-    }
+    println!("Done finding escape velocity, now colouring by numbers.");
 
-    // Stitches output regions back together.
-    let mut master_image = RgbImage::new(output_region.w, output_region.h);
-    for _ in 0..no_regions {
-        let (region, img) = rx.recv().unwrap();
-        replace(&mut master_image, &img, region.x, region.y);
-    }
-
-    // Rejoins with worker threads.
-    for handle in handles {
-        handle.join().unwrap();
+    // Not really worth parallelising seeing as it is so quick.
+    for pixel in master_image.pixels_mut() {
+        //todo: optimisation to stop using pop here below.
+        if let Some(result) = results.pop() {
+            let colour = match result {
+                MandelResult::Outside(i) => {
+                    let components = grad.get((i / 25.0) % 1.0);
+                    image::Rgb([
+                        (components.red * 255.0) as u8,
+                        (components.green * 255.0) as u8,
+                        (components.blue * 255.0) as u8,
+                    ])
+                }
+                MandelResult::Inside => image::Rgb([0u8, 0u8, 0u8]),
+            };
+            *pixel = colour;
+        }
     }
 
     // Saves resultant image to disk.
--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,8 +1,15 @@
+//todo: reduce the size of `MandelResult` by using NaN or inf to represent the variant `Inside`.
+
 use num_complex::Complex;
 
+const POWER: f64 = 2.0;
 const MAX_ITERATIONS: u8 = 255;
 //Apparently this only needs to be 2.0, thanks Ben Sparks from Numberphile.
+#[cfg(not(feature = "smoothing"))]
 const MAX_MAGNITUDE: f64 = 2.0;
+// But a number so low does mess with the smoothing algorithm.
+#[cfg(feature = "smoothing")]
+const MAX_MAGNITUDE: f64 = 1024.0;
 
 pub enum MandelResult {
     Inside,
@@ -13,15 +20,15 @@ pub fn iterate_mandelbrot(c: Complex<f64>) -> MandelResult {
     let mut x = Complex::new(0.0, 0.0);
 
     for i in 0..MAX_ITERATIONS {
-        x = x.powf(2.0) + c;
+        x = x.powf(POWER) + c;
 
         if x.norm_sqr() > MAX_MAGNITUDE.powi(2) {
             // Removing the smoothing does not seem to save any time,
             // but it does look nicer with this gradient, no?
-            //let i = i as f64 + 1.0 - x.norm().ln().log(MAX_MAGNITUDE);
+            #[cfg(feature = "smoothing")]
+            let i = i as f64 - (x.norm_sqr() / 2.0).log(MAX_MAGNITUDE).log(POWER);
 
-            //todo: move this constant offset onto the gradient.
-            return MandelResult::Outside(i as f64 - 2.75);
+            return MandelResult::Outside(i as f64);
         }
     }
 
